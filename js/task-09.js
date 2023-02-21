262becb08e6adda495cc527f9cbb41222c2a1a3e
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-02-22 00:16:14 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+
+const colorChange = () => {
+  let randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+};
+
+changeColorBtn.addEventListener("click", colorChange);
