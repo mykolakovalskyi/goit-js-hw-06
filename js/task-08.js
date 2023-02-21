aa68e46d8dff7b935c418b06ef59cab002c2602b
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-02-22 00:05:15 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Wszystkie pola powinny zostać wypełnione!");
+  }
+
+  console.log({ email: `${email.value}`, password: `${password.value}` });
+  event.currentTarget.reset();
+};
+
+loginForm.addEventListener("submit", handleSubmit);
