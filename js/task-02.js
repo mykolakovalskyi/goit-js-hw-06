e197b02c25d6de8d6b8e61e6af66839ced5e1a11
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-02-21 21:27:36 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listOfIngredients = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  let tag = document.createElement("li");
+  tag.textContent = `${ingredient}`;
+  tag.classList.add("item");
+  listOfIngredients.append(tag);
+});
