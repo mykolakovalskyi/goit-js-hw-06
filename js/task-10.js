26bb3c61b026a208a0a5411dbda610cb5e047943
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-02-22 02:49:30 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+const createBoxes = (amount) => {
+  const elementsToAdd = [];
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.height = `${30 + 10 * i}px`;
+    div.style.width = `${30 + 10 * i}px`;
+    div.style.background = getRandomHexColor();
+    elementsToAdd.push(div);
+  }
+  return elementsToAdd;
+};
+
+const destroyBoxes = () => {
+  boxes.innerHTML = "";
+};
+
+createBtn.addEventListener("click", () => {
+  let boxesToAdd = createBoxes(input.value);
+  boxes.append(...boxesToAdd);
+});
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes.call();
+});
